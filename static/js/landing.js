8fcb4cab777d4195341ab5a5f8e712ea182f8e7b
ss031672 <Siddharth.Shanbhogue@cerner.com>
2019-01-01 00:36:27 +0530

    Added form and validation

--- static/js/landing.js
+++ static/js/landing.js
@@ -23,6 +23,9 @@ $(document).ready(function () {
             .removeClass("btn-secondary")
             .addClass("btn-secondary");
 
+        $("#organization").text("Name of your college");
+        $("#designation").text("College department");
+
         console.log(state);
 
     });
@@ -42,8 +45,25 @@ $(document).ready(function () {
             .removeClass("btn-secondary")
             .addClass("btn-secondary");
 
+        $("#organization").text("Name of your organization");
+        $("#designation").text("Your designation");
+
         console.log(state);
 
     });
-
 });
+
+window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener('submit', function (event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+}, false);
