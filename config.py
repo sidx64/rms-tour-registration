196ae13b2bb39ade147b81a258b4be6f09ab9806
ss031672 <Siddharth.Shanbhogue@cerner.com>
2019-01-01 16:28:38 +0530

    Added smtp server abstraction in config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# THIS FILE SHOULD NOT BE PUSHED TO GIT
+
+# Config Items
+username = 'email'
+password = 'password'
+smtp_server = 'smtp.gmail.com'
+port = '465'
--- mailCode.py
+++ mailCode.py
@@ -2,22 +2,23 @@
 # your Gmail account
 
 
-def send_email(user , pwd , recipient , body, subject):
+def send_email(user, pwd, recipient, body, subject):
     import smtplib
+    import config
 
     FROM = user
-    TO = recipient if isinstance(recipient , list) else [recipient]
+    TO = recipient if isinstance(recipient, list) else [recipient]
     SUBJECT = subject
     TEXT = body
 
     # Prepare actual message
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s
-    """ % (FROM , ", ".join(TO) , SUBJECT , TEXT)
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
     try:
-        server = smtplib.SMTP_SSL("smtp.gmail.com" , 465)
+        server = smtplib.SMTP_SSL(config.smtp_server, config.port)
         server.ehlo()
-        server.login(user , pwd)
-        server.sendmail(FROM , TO , message)
+        server.login(user, pwd)
+        server.sendmail(FROM, TO, message)
         server.close()
         print('successfully sent the mail')
 
